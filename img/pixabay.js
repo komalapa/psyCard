fd83b81b4317d322c8b8b60f412d1a74aea61b76
komalapa <komalapa@gmail.com>
2021-03-19 17:34:58 +0400

    Колоды с тестовыми картинками с pixabay

---
+++ img/pixabay.js
@@ -0,0 +1,110 @@
+  const imgsPix1 = [
+      "./img/cards_from_pixabay/tinified-1/1.png",
+      "./img/cards_from_pixabay/tinified-1/2.png",
+      "./img/cards_from_pixabay/tinified-1/3.png",
+      "./img/cards_from_pixabay/tinified-1/4.png",
+      "./img/cards_from_pixabay/tinified-1/5.png",
+      "./img/cards_from_pixabay/tinified-1/6.png",
+      "./img/cards_from_pixabay/tinified-1/7.png",
+      "./img/cards_from_pixabay/tinified-1/1.png",
+      "./img/cards_from_pixabay/tinified-1/2.png",
+      "./img/cards_from_pixabay/tinified-1/3.png",
+      "./img/cards_from_pixabay/tinified-1/4.png",
+      "./img/cards_from_pixabay/tinified-1/5.png",
+      "./img/cards_from_pixabay/tinified-1/6.png",
+      "./img/cards_from_pixabay/tinified-1/7.png",
+      "./img/cards_from_pixabay/tinified-1/1.png",
+      "./img/cards_from_pixabay/tinified-1/2.png",
+      "./img/cards_from_pixabay/tinified-1/3.png",
+      "./img/cards_from_pixabay/tinified-1/4.png",
+      "./img/cards_from_pixabay/tinified-1/5.png",
+      "./img/cards_from_pixabay/tinified-1/6.png",
+      "./img/cards_from_pixabay/tinified-1/7.png",
+      "./img/cards_from_pixabay/tinified-1/1.png",
+      "./img/cards_from_pixabay/tinified-1/2.png",
+      "./img/cards_from_pixabay/tinified-1/3.png",
+      "./img/cards_from_pixabay/tinified-1/4.png",
+      "./img/cards_from_pixabay/tinified-1/5.png",
+      "./img/cards_from_pixabay/tinified-1/6.png",
+      "./img/cards_from_pixabay/tinified-1/7.png",
+      "./img/cards_from_pixabay/tinified-1/1.png",
+      "./img/cards_from_pixabay/tinified-1/2.png",
+      "./img/cards_from_pixabay/tinified-1/3.png",
+      "./img/cards_from_pixabay/tinified-1/4.png",
+      "./img/cards_from_pixabay/tinified-1/5.png",
+      "./img/cards_from_pixabay/tinified-1/6.png",
+      "./img/cards_from_pixabay/tinified-1/7.png",
+  ]
+  addDeck("Карты pixabay1","pixa1", imgsPix1);
+
+  const imgsPix2 = [
+    "./img/cards_from_pixabay/tinified-2/1.png",
+    "./img/cards_from_pixabay/tinified-2/2.png",
+    "./img/cards_from_pixabay/tinified-2/3.png",
+    "./img/cards_from_pixabay/tinified-2/4.png",
+    "./img/cards_from_pixabay/tinified-2/5.png",
+    "./img/cards_from_pixabay/tinified-2/6.png",
+    
+    "./img/cards_from_pixabay/tinified-2/1.png",
+    "./img/cards_from_pixabay/tinified-2/2.png",
+    "./img/cards_from_pixabay/tinified-2/3.png",
+    "./img/cards_from_pixabay/tinified-2/4.png",
+    "./img/cards_from_pixabay/tinified-2/5.png",
+    "./img/cards_from_pixabay/tinified-2/6.png",
+    
+    "./img/cards_from_pixabay/tinified-2/1.png",
+    "./img/cards_from_pixabay/tinified-2/2.png",
+    "./img/cards_from_pixabay/tinified-2/3.png",
+    "./img/cards_from_pixabay/tinified-2/4.png",
+    "./img/cards_from_pixabay/tinified-2/5.png",
+    "./img/cards_from_pixabay/tinified-2/6.png",
+    
+    "./img/cards_from_pixabay/tinified-2/1.png",
+    "./img/cards_from_pixabay/tinified-2/2.png",
+    "./img/cards_from_pixabay/tinified-2/3.png",
+    "./img/cards_from_pixabay/tinified-2/4.png",
+    "./img/cards_from_pixabay/tinified-2/5.png",
+    "./img/cards_from_pixabay/tinified-2/6.png",
+    
+    "./img/cards_from_pixabay/tinified-2/1.png",
+    "./img/cards_from_pixabay/tinified-2/2.png",
+    "./img/cards_from_pixabay/tinified-2/3.png",
+    "./img/cards_from_pixabay/tinified-2/4.png",
+    "./img/cards_from_pixabay/tinified-2/5.png",
+    "./img/cards_from_pixabay/tinified-2/6.png",
+    
+]
+//addDeck("Карты pixabay2","pixa2", imgsPix2);
+
+const imgsPix3 = [
+  "./img/cards_from_pixabay/tinified-3/1.jpg",
+  "./img/cards_from_pixabay/tinified-3/2.png",
+  "./img/cards_from_pixabay/tinified-3/3.jpg",
+  "./img/cards_from_pixabay/tinified-3/4.jpg",
+  
+  "./img/cards_from_pixabay/tinified-3/1.jpg",
+  "./img/cards_from_pixabay/tinified-3/2.png",
+  "./img/cards_from_pixabay/tinified-3/3.jpg",
+  "./img/cards_from_pixabay/tinified-3/4.jpg",
+  "./img/cards_from_pixabay/tinified-3/1.jpg",
+  "./img/cards_from_pixabay/tinified-3/2.png",
+  "./img/cards_from_pixabay/tinified-3/3.jpg",
+  "./img/cards_from_pixabay/tinified-3/4.jpg",
+  "./img/cards_from_pixabay/tinified-3/1.jpg",
+  "./img/cards_from_pixabay/tinified-3/2.png",
+  "./img/cards_from_pixabay/tinified-3/3.jpg",
+  "./img/cards_from_pixabay/tinified-3/4.jpg",
+  "./img/cards_from_pixabay/tinified-3/1.jpg",
+  "./img/cards_from_pixabay/tinified-3/2.png",
+  "./img/cards_from_pixabay/tinified-3/3.jpg",
+  "./img/cards_from_pixabay/tinified-3/4.jpg",
+  
+]
+addDeck("Дамы pixabay","pixadama", imgsPix3);
+
+const imgsPix4 = [
+  "./img/cards_from_pixabay/tinified-4/1.jpg",
+  "./img/cards_from_pixabay/tinified-4/2.jpg",
+  "./img/cards_from_pixabay/tinified-4/3.jpg",
+]
+addDeck("Цветы pixabay","pixaflower", imgsPix4,"120px", "170px", 1, true);
